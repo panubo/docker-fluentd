2fe935e6c4a6509a5ce736a808de95deca6668b8
Tim Robinson <tim@panubo.com>
2022-07-26 17:49:36 +1000

    Upgrade fluentd and gems

--- debian/update.sh
+++ debian/update.sh
@@ -7,10 +7,10 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
   osx_compat="g"
 fi
 
+# Skip image upgrade since 1.15 uses ruby 3 which some gems don't yet support
+# latest="$(skopeo inspect docker://docker.io/fluent/fluentd | jq .RepoTags[] -r | sort -V | grep 'debian-1' | tail -n1)"
 
-latest="$(skopeo inspect docker://docker.io/fluent/fluentd | jq .RepoTags[] -r | sort -V | grep 'debian-1' | tail -n1)"
-
-${osx_compat}sed -i -E -e "s/^FROM.*/FROM fluent\/fluentd:${latest}/" Dockerfile Dockerfile.update
+# ${osx_compat}sed -i -E -e "s/^FROM.*/FROM fluent\/fluentd:${latest}/" Dockerfile Dockerfile.update
 
 docker build -f Dockerfile.update -t fluentd-gemfile-lock .
 docker create --name fluentd-gemfile-lock fluentd-gemfile-lock
